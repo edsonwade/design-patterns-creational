86bf13b7ab02f7ffcc6edfc4f8b1f2eaf05d8860
vanilson muhongo <vanilsonmuhongo@gmail.com>
2024-08-08 20:05:47 +0100

    Project init

---
+++ src/main/java/code/with/vanilson/App.java
@@ -0,0 +1,10 @@
+package code.with.vanilson;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/code/with/vanilson/singleton/Singleton.java
@@ -0,0 +1,24 @@
+package code.with.vanilson.singleton;
+
+/**
+ * Singleton
+ *
+ * @author vamuhong
+ * @version 1.0
+ * @since 2024-08-08
+ */
+public class Singleton {
+
+    public static void main(String[] args) {
+        Runtime singletonRuntime = Runtime.getRuntime();
+        singletonRuntime.gc();
+        System.out.println(singletonRuntime); // java.lang.Runtime@6ce253f1
+
+        Runtime anotherInstance = Runtime.getRuntime();
+        System.out.println(anotherInstance); //java.lang.Runtime@6ce253f1
+
+        if (singletonRuntime == anotherInstance) {
+            System.out.println("They are the same instance");
+        }
+    }
+}
